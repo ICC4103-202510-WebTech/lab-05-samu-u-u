59bd9eade3eae8e3fc7ae3370bca08d2c3be283d
samu-u <sivallette@miuandes.cl>
2025-05-30 19:25:27 -0400

    authorization

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,6 @@
 class ChatsController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     @chats = Chat.all
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,10 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :read, :all
+    can :read, Chat, sender_id: user.id
+    can :read, Chat, receiver_id: user.id
+    can :create, Chat
+    can :read, Message
     can :create, Message
     can [:update, :destroy], Message, user_id: user.id
   end
