6235691a75d50f19ea386586a9364afc660af1f9
samu-u <sivallette@miuandes.cl>
2025-05-30 19:58:12 -0400

    last commit lab-09

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,9 +10,8 @@ class Ability
     can :read, Chat, sender_id: user.id
     can :read, Chat, receiver_id: user.id
     can :create, Chat
-    can :read, Message
     can :create, Message
-    can [:update, :destroy], Message, user_id: user.id
+    can [:read, :update, :destroy], Message, user_id: user.id
   end
     # Define abilities for the user here. For example:
     #
